ee198c1f9d2e01666933b374163f9b6e8f234db0
SilasNight <15emiliomurray@gmail.com>
2025-03-07 11:37:01 +0200

    wrote to file with date as the name and the starting line

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 import datetime
-print("What happened today?")
-day_lines = []
-date = datetime.datetime.now()
-date = datetime.datetime.strftime(date,"%d-%m-%Y")
-print(date)
-def main():
-    while True:
-        write()
 
 
-def write():
-    while True:
-        action = input()
-        if action == "Exit":
-            pass
+class Diary:
+    def __init__(self):
+
+        self.wrote = "no"
+        print("What happened today?")
+        self.day_lines = []
+        date = datetime.datetime.now()
+        self.date = datetime.datetime.strftime(date,"%d-%m-%Y")
+        self.path = f"Diary/{self.date}.txt"
+        while True:
+            query = input("Do you have something to write?")
+            if query == "yes":
+                self.write()
+            elif query == "no":
+                if self.wrote == "yes":
+                    self.save()
+                break
+
+
+
+
+    def write(self):
+        self.day_lines = [self.date]
+        while True:
+            action = input()
+            if action == "Exit":
+                self.wrote = "yes"
+                break
+            else:
+                self.day_lines.append(action)
+
+    def save(self):
+        exist = self.check_file()
+        with open(self.path,exist) as file:
+            file.writelines(self.day_lines)
+
+    def check_file(self):
+        try:
+            with open(self.path,"r") as file:
+                file.readlines()
+        except FileNotFoundError:
+            return "w"
         else:
-            day_lines.append(action)
+            return "a"
 
-main()
+Diary()
